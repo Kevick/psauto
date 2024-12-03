85a0c4a7e24374e3a469903adceb608b25796f80
Tungs <jairneto180801@gmail.com>
2024-12-03 12:51:00 -0300

    fix directory

--- src/page/psauto-website.jsx
+++ src/page/psauto-website.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Gauge, Send, CheckCircle, Wrench, BarChart } from 'lucide-react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Gallery from './components/Gallery';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
+import Header from '../components/Header'
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Gallery from '../components/Gallery';
+import ContactForm from '../components/ContactForm';
+import Footer from '../components/Footer';
 
 const PsautoWebsite = () => {
   return (
