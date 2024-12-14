13a2f3a75b16aca6d11221b9cbff56eb56173a0c
Kevick <jairneto180801@gmail.com>
2024-12-14 20:37:33 -0300

    icon e galery

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -9,7 +9,7 @@ const ImageGallery = () => {
       <div className="container mx-auto">
         {/* Instagram Feed com Grid */}
         <div className="mt-12">
-          <h3 className="text-3xl text-center mb-8 text-red-500">Nosso Instagram</h3>
+          <h3 className="text-3xl text-center mb-8 text-red-500">Nossos Trabalhos</h3>
           {/* Estilo do Grid para o Feed do Instagram */}
           <div className="elfsight-widget" style={{ width: '100%', height: 'auto' }}>
             <ElfsightWidget widgetID="7f6d9182-bb2f-425d-a7cb-695d39328658" />
