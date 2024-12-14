01e4dee2013becb3194be86436a9512fef0c40c5
Kevick <jairneto180801@gmail.com>
2024-12-14 20:29:55 -0300

    instagram final

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
+import { ElfsightWidget } from 'react-elfsight-widget';
 
 // Importa dinamicamente todas as imagens na pasta src/imgs
-const importarImagens = (caminho) => {
-  let imagens = {};
-  caminho.keys().forEach((item, index) => {
-    imagens[item.replace('./', '')] = caminho(item);
-  });
-  return imagens;
-};
-
-const imagens = importarImagens(require.context('../img', false, /\.(png|jpe?g|svg)$/));
 
 const ImageGallery = () => {
   return (
-    <section id='nosso-trabalho' className="py-16 bg-gray-800">
+    <section id="nosso-trabalho" className="py-16 bg-gray-800">
       <div className="container mx-auto">
-        <h3 className="text-3xl text-center mb-12 text-red-500">Nosso Trabalho</h3>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {Object.entries(imagens).map(([nome, caminho], index) => (
-            <div key={index} className="overflow-hidden rounded-lg">
-              <img 
-                src={caminho} 
-                alt={`Preparação ${index + 1}`} 
-                className="w-full h-full object-cover hover:scale-110 transition-transform"
-              />
-            </div>
-          ))}
+        {/* Instagram Feed com Grid */}
+        <div className="mt-12">
+          <h3 className="text-3xl text-center mb-8 text-red-500">Nosso Instagram</h3>
+          {/* Estilo do Grid para o Feed do Instagram */}
+          <div className="elfsight-widget" style={{ width: '100%', height: 'auto' }}>
+            <ElfsightWidget widgetID="7f6d9182-bb2f-425d-a7cb-695d39328658" />
+          </div>     
         </div>
       </div>
     </section>
